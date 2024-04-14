1cc1ec3e17bbf4fe0249347dc00e492bab84da13
avneetahuja <aahuja16@asu.edu>
2024-04-14 03:38:19 -0700

    added convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
 import Image from "next/image";
+import { Avatar,AvatarImage,AvatarFallback } from "@/components/ui/avatar";
 
 export default function Home() {
+  const [preview, setPreview] = useState("");
   return (
-    <div>
-      Hello World
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      {/* <Avatar className="w-24 h-24">
+            <AvatarImage src={preview} />
+            <AvatarFallback>BU</AvatarFallback>
+          </Avatar> */}
+          <Image src={preview? preview : ""} alt="icon" width={500} height={200} className="rounded-full"/>
+      <Label htmlFor="image"></Label>
+      <Input
+        id="image"
+        type="file"
+        onChange={(event) => {
+          const dataTransfer = new DataTransfer();
+          Array.from(event.target.files!).forEach((image) =>
+            dataTransfer.items.add(image)
+          );
+          const files = dataTransfer.files;
+          const displayUrl = URL.createObjectURL(event.target.files![0]);
+          setPreview(displayUrl);
+          // onChange(files);
+        }}
+      ></Input>
+      
     </div>
   );
 }
